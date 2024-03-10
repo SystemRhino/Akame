403374a97a96fea96b98f21a061f89b2c608d492
SystemRhino <80726377+SystemRhino@users.noreply.github.com>
2024-03-09 23:00:40 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include('./php/conecta.php');
+?>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <link rel="stylesheet" href="./css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="./img/logo-gif.gif" alt="">
+    <h1>[ENTER]</h1>
+</html>
