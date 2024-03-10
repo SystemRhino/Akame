dfeff4dd0728fe74fca3229045164101075bdd28
SystemRhino <80726377+SystemRhino@users.noreply.github.com>
2024-03-10 00:54:37 -0300

    Add files via upload

---
+++ php/conecta.php
@@ -0,0 +1,10 @@
+<?php 
+try {
+  $conn = new PDO('mysql:host=127.0.0.1:3306;dbname=bd_akame', 'root', '');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $conn -> exec("SET CHARACTER SET utf8");
+} catch(PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
+?>
