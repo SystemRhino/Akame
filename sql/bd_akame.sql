2caab55f7432c019b61fb05efeb1e16e7b61827d
SystemRhino <80726377+SystemRhino@users.noreply.github.com>
2024-03-10 00:54:10 -0300

    Add files via upload

---
+++ sql/bd_akame.sql
@@ -0,0 +1,65 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema bd_akame
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema bd_akame
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `bd_akame` DEFAULT CHARACTER SET utf32 ;
+USE `bd_akame` ;
+
+-- -----------------------------------------------------
+-- Table `bd_akame`.`tb_categoria`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bd_akame`.`tb_categoria` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nm_categoria` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf32;
+
+
+-- -----------------------------------------------------
+-- Table `bd_akame`.`tb_products`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bd_akame`.`tb_products` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nm_produto` VARCHAR(45) NULL DEFAULT NULL,
+  `id_categoria` INT(11) NULL DEFAULT NULL,
+  `vl_produto` VARCHAR(45) NULL DEFAULT NULL,
+  `ds_produto` VARCHAR(45) NULL DEFAULT NULL,
+  `ds_img` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `bd_akame`.`tb_users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bd_akame`.`tb_users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nm_user` VARCHAR(45) NULL DEFAULT NULL,
+  `ds_email` VARCHAR(45) NULL DEFAULT NULL,
+  `ds_tel` VARCHAR(45) NULL DEFAULT NULL,
+  `ds_senha` VARCHAR(45) NULL DEFAULT NULL,
+  `ds_adress` VARCHAR(200) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf32;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
