2e6b3867bacd8478eadf1ff6e453f390dfe86a46
SystemRhino <80726377+SystemRhino@users.noreply.github.com>
2024-03-24 22:51:26 -0300

    Add files via upload

--- php/script_carrinho.php
+++ php/script_carrinho.php
@@ -1,12 +1,17 @@
 <?php
 session_start();
+if (!isset($_SESSION['id'])){
+    header('location:../login.php');
+}else{
 $id_user = $_SESSION['id'];
+$id_produto = $_GET['id'];
     include('conecta.php');
         $stmt = $conn->prepare('INSERT INTO tb_carrinho(id_user, id_produto) VALUES(:id_user, :id_produto)');
         $result = $stmt->fetch(PDO::FETCH_ASSOC);   
         $stmt->execute(array(
             ':id_user' => $id_user,
-            ':id_produto' => $_POST['id_produto']
+            ':id_produto' => $id_produto
         ));
-        echo "<meta HTTP-EQUIV='refresh' CONTENT='0'>";
+        echo "<script>history.go(-1);</script>";
+    }
 ?>
