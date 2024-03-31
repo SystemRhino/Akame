c705b5c7281881d0fea60196357e45b44c3d73f0
Amália Coelho <86680741+amalia-coelho@users.noreply.github.com>
2024-03-30 22:26:19 -0300

    Update produto.php

--- produto.php
+++ produto.php
@@ -1,123 +1,76 @@
-<?php 
-include('./php/conecta.php');
-
-if(isset($_GET['id'])){
-    $nm_produto = $_GET['id'];
-    $script_produtos = $conn->prepare("SELECT * FROM tb_products WHERE  nm_produto = '$nm_produto'");
-    $script_produtos->execute();
-    $produto = $script_produtos->fetch(PDO::FETCH_ASSOC);
+<?php
+  if(!isset($_GET['id'])){
+      header('location: catalogo.php');
+  }else{
+    include('php/conecta.php');
+    $caminho_img = $_GET['id'];
+    $script_produtos = $conn->prepare("SELECT * FROM tb_products WHERE  img_1 = '$caminho_img'");
+    $script_produtos->execute(); 
+    if($script_produtos->rowCount() == 0){
+      header("Location: catalogo.php");
     }else{
-        header('location:catalogo.php');
-    }
+      $produto = $script_produtos->fetch(PDO::FETCH_ASSOC);
 ?>
-
 <!DOCTYPE html>
-<html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/footer.css">
-    <link rel="stylesheet" href="css/navbar.css">
-    <link rel="stylesheet" href="css/index.css">
-    <link rel="stylesheet" href="css/produto.css">
+<html lang="pt-br">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="css/navbar.css">
+      <link rel="stylesheet" href="css/produto.css">
+      <link rel="stylesheet" href="css/footer.css">
 
-
-
-    <title><?php echo $nm_produto;?></title>
-
-    <span></span>
-    	<!-- JS -->
-	<!--<script src="js/jquery-3.6.0.min.js"></script>
-	<script type="text/javascript">
-		$(document).ready(function(){
-			$("#cart").click(function(){
-  			$.ajax({
-  				url: "php/script_carrinho.php",
-  				type: "POST",
-  				data: "id_produto=<?php echo $produto['id']?>",
-  				dataType: "html"
-  			}).done(function(resposta) {
-	    $("span").html(resposta);
-
-		}).fail(function(jqXHR, textStatus ) {
-	    console.log("Request failed: " + textStatus);
-
-		}).always(function() {
-	    console.log("completou");
-		});
-  	});
-});
-	</script>-->
-</head>
-<body>
-<?php include('php/navbar.php');?>
-<div class="main">
-    <aside class="left"> 
-    
-    </aside>
-
+      <title><?php echo $produto['nm_produto'];?></title>
+  </head>
+  <body>
+    <?php include('php/navbar.php');?>
     <main>
-
-    <?php
-// Verificação se tem produtos
-if ($script_produtos->rowCount()>0){
-?>
-
-
-    <div class = "card-wrapper">
-      <div class = "card">
-        <!-- card left -->
-        <div class = "product-imgs">
-          <div class = "img-display">
-            <div class = "img-showcase">
-            <img src="./img/<?php echo $produto['img_1'];?>" alt="shoe image">
-              <!--<img src = "https://images.yampi.me/assets/stores/atlas-company2/uploads/images/camiseta-a-bhating-ape-are-shall-never-kill-ape-preto-65a009359dfea-large.png" alt = "shoe image">-->
-       
+        <div class = "card">
+             <!-- card left -->
+            <div class = "product-imgs">
+                <div class = "img-display">
+                    <div class = "img-showcase">
+                        <img src="img/<?php echo $produto['img_1'];?>" alt="shoe image">
+                    </div>
+                </div>
             </div>
-          </div>
-        
+            <!-- card right -->
+            <div class="product-content">
+                <h2 class="product-title"><?php echo $produto['nm_produto'];?></h2>
+                <div class="product-price">
+                    <p class="new-price">R$ <?php echo $produto['vl_produto'];?>.00</p>
+                </div>
+                <div class="product-detail">
+                    <h2>sobre o item:</h2>
+                    <p><?php echo $produto['ds_produto'];?></p>           
+                </div>
+                <div class="purchase-info">
+                    <input type="number" min="1" value="1" id="quant">
+                    <button id="cart" type="button" class="btn" onclick="addCarrinho();">Add to Cart <i class="fas fa-shopping-cart"></i></button>
+                    <button onclick="window.location.href='checkout.php?id=<?php echo $nm_produto;?>'" type = "button" class="btn">Comprar</button>
+                </div>
+            </div>
         </div>
-        <!-- card right -->
-        <div class = "product-content">
-          <h2 class = "product-title"><?php echo $produto['nm_produto'];?></h2>
-         
-          <div class = "product-price">
-            <p class = "new-price">New Price: <span>$249.00 (5%)</span></p>
-          </div>
-
-          <div class = "product-detail">
-            <h2>sobre o item:</h2>
-<p><?php echo $produto['ds_produto'];?></p>           
-          </div>
-
-          <div class = "purchase-info">
-            <input type = "number" min = "0" value = "1">
-            <button id="cart" type = "button" class = "btn" onclick="window.location.href = './php/script_carrinho.php?id=<?php echo $produto['id'];?>'">
-              Add to Cart <i class = "fas fa-shopping-cart"></i>
-            </button>
-            
-            <button onclick="window.location.href = './php/script_carrinho.php?id=<?php echo $produto['id'];?>'" type = "button" class = "btn">Comprar</button>
-
-          </div>
-
-          
-        </div>
-      </div>
-    </div>
-    
-    <?php        
-        }else{
-            // Caso não tenha produto
-            header('location:catalogo.php');
-        }
-    ?>
-
     </main>
-<aside class="right"></aside> 
-    </div>
-
-
     <?php include('php/footer.php');?>
 
-</body>
+    <script>
+      function addCarrinho() {
+        // Obtém o valor do input
+        var quantidade = document.getElementById('quant').value;
+
+        // Obtém o ID do produto da URL atual
+        var url = window.location.href;
+        var parametros = new URLSearchParams(window.location.search);
+        var idProduto = parametros.get('id');
+
+        // Monta a URL com os parâmetros adicionados
+        var novaURL = 'php/script_carrinho.php?id=' + idProduto + '&quant=' + quantidade;
+
+        // Redireciona para a nova URL
+        window.location.href = novaURL;
+      }
+    </script>
+  </body>
 </html>
+<?php } } ?>
